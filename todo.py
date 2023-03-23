fda950bfb7f4fced379e2ea32506198a97197fe9
Davi <daviibrito111@gmail.com>
2023-03-23 00:46:40 -0300

    lists, select, create and delete commands working

--- todo.py
+++ todo.py
@@ -1,6 +1,7 @@
 import cmd
 from rich.table import Table
 from task import Task
+from database import getTablesNames, createTable, tableExists, deleteTable
 
 class MyCLI(cmd.Cmd):
     def __init__(self):
@@ -13,20 +14,55 @@ class MyCLI(cmd.Cmd):
             "q" : self.do_quit,
             "h" : self.do_help
         }
+        self.actual_list = None
 
     def do_lists(self, args):
-        pass
+        """Lists all available TODO lists"""
+        lists = getTablesNames()
+        for list in lists:
+            print(list)
 
-    def do_select(self, arg):
-        pass
+    def do_select(self, list_name):
+        """Selects a list to interact with"""
+        if not tableExists(list_name):
+            print("Invalid list name!")
+            return 
+        
+        self.actual_list = list_name
+        print(f"Selected list {list_name}")
 
     def do_show(self, arg):
         pass
 
+    def do_create(self, list_name):
+        """Add a new TODO list"""
+        list_name = list_name.split()[0]
+
+        if tableExists(list_name):
+            print("List already exists!")
+            return
+
+        createTable(list_name)
+        self.do_lists(list_name)   
+
+    def do_delete(self, list_name):
+        """Deletes a TODO list"""
+        list_name = list_name.split()[0]
+
+        if not tableExists(list_name):
+            print("List does not exists!")
+            return
+        
+        deleteTable(list_name)
+
+        self.do_lists(list_name) 
+
     def do_add(self, args):
+        """Adds a TODO to the list"""
         pass
 
     def do_remove(self, arg):
+        """Removes a TODO from the list"""
         pass
 
     def do_done(self, arg):
