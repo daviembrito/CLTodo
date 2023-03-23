e7323d4b500de80acb2883802ccd3c3a706c3c9f
Davi <daviibrito111@gmail.com>
2023-03-23 00:45:24 -0300

    Added new functions

--- database.py
+++ database.py
@@ -1,24 +1,47 @@
 import sqlite3
+from task import Task
 
-# Conecta ao banco de dados
 conn = sqlite3.connect('todos.db')
 cursor = conn.cursor()
 
-def getTables():
-    number_of_tables = getNumberOfTables()
-    if (number_of_tables == 0):
-        return ["No lists found"]
+def getTablesNames():
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name!='sqlite_sequence';")
+    tables = cursor.fetchall()
+    if not tables:
+        return ["No list found!"]
     
-    return number_of_tables
+    tables_list = [table[0] for table in tables]
+    return tables_list
 
 def getNumberOfTables():
-    cursor.execute("SELECT count(*) FROM sqlite_master WHERE type='table'")
+    cursor.execute("""SELECT count(*) FROM sqlite_master 
+            WHERE type='table' 
+            AND name!='sqlite_sequence';""")
     num_tables = cursor.fetchone()[0]
     return num_tables
+
+def createTable(table_name):
+    cursor.execute(f"""CREATE TABLE IF NOT EXISTS {table_name} (
+            id integer primary key autoincrement,
+            todo text,
+            category text,
+            date_added text,
+            date_completed text,
+            done integer);""")
     
+def deleteTable(table_name):
+    cursor.execute(f"DROP TABLE {table_name}")
+    
+def addTaskToTable(task : Task, table_id : int):
+    pass
 
-# Exibe o número de tabelas
-print(f"Existem {getNumberOfTables()} tabelas na base de dados")
-
-# Fecha a conexão
-conn.close()
+def tableExists(table_name):
+    cursor.execute(f"""SELECT name FROM sqlite_master
+            WHERE type='table' 
+            AND name='{table_name}';""")
+    
+    result = cursor.fetchone()
+    if result:
+        return True
+    
+    return False
