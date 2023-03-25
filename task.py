93c42fc1e36c29e927b31137dcd3fabb1a305f63
Davi <daviibrito111@gmail.com>
2023-03-25 02:47:35 -0300

    Changed datetime to include miliseconds

--- task.py
+++ task.py
@@ -4,6 +4,6 @@ class Task():
     def __init__(self, name, category):
         self.name = name
         self.category = category
-        self.created_date = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+        self.created_date = datetime.now().strftime("%d/%m/%Y, %H:%M:%S.%f")
         self.done = False
         self.done_date = None
