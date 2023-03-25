a3fc4e67d020ab185ed6dba013fa63a3e77bbd46
Davi <daviibrito111@gmail.com>
2023-03-25 15:14:52 -0300

    Added a second constructor for less parameters

--- task.py
+++ task.py
@@ -1,9 +1,18 @@
 from datetime import datetime
 
 class Task():
-    def __init__(self, name, category):
+    def __init__(self, position, name, category, created_date, done_date, done):
+        self.position = position
         self.name = name
         self.category = category
-        self.created_date = datetime.now().strftime("%d/%m/%Y, %H:%M:%S.%f")
+        self.created_date = created_date
         self.done = False
         self.done_date = None
+
+    @classmethod
+    def create(cls, name, category):
+        position = -1
+        created_date = datetime.now().strftime("%d/%m/%Y, %H:%M:%S.%f")
+        done = False
+        done_date = None
+        return cls(position, name, category, created_date, done_date, done)
