843626aa41fd778eece74e77fdec253f3ade4291
Davi <daviibrito111@gmail.com>
2023-03-27 11:03:24 -0300

    Correct Task constructor

--- task.py
+++ task.py
@@ -1,16 +1,16 @@
 from datetime import datetime
 
 class Task():
-    def __init__(self, position, name, category, created_date, done_date, done):
+    def __init__(self, position:int, name:str, category:str, created_date:str, done_date:str, done:bool):
         self.position = position
         self.name = name
         self.category = category
         self.created_date = created_date
-        self.done = False
-        self.done_date = None
+        self.done = done
+        self.done_date = done_date
 
     @classmethod
-    def create(cls, name, category):
+    def create(cls, name:str, category:str):
         position = -1
         created_date = datetime.now().strftime("%d/%m/%Y, %H:%M:%S.%f")
         done = False
