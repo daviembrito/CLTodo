ef6ce6f3e1bcf1cc73dbe67c568cf9c5c743c0d7
Davi <daviibrito111@gmail.com>
2023-03-28 01:48:20 -0300

    Better help and error message

--- CLTodo.py
+++ CLTodo.py
@@ -26,21 +26,27 @@ class TodoCLI(cmd.Cmd):
     def do_lists(self, args):
         """Lists all available TODO lists"""
         lists = db.getTablesNames()
+
+        if lists == ["No list found!"]:
+            self.printErrorMessage(lists[0])
+            return
+
         for list in lists:
             print(list)
 
     def do_select(self, list_name):
         """Selects a list to interact with"""
         if not db.tableExists(list_name):
-            print("Invalid list name!")
+            self.printError("Invalid list name!")
             return 
         
         self.selected_list = list_name
-        print(f"Selected list {list_name}!")
+        self.printMessage(f"Selected list {list_name}!")
 
     def do_show(self, category):
+        """Shows the list"""
         if not self.hasSelectedList():
-            self.printSelectError()
+            self.printError("You must select a list before!")
             return
         
         if category:
@@ -49,25 +55,11 @@ class TodoCLI(cmd.Cmd):
         else:
             todos = db.getAllRows(self.selected_list)
 
-        table = Table(show_header=True, header_style="bold cyan")
-        table.add_column("#", style="dim", width=4, justify="center")
-        table.add_column("Todo", min_width=20, justify="center")
-        table.add_column("Category", min_width=12, justify="center")
-        table.add_column("Created At", min_width=12, justify="center")
-        table.add_column("Done At", min_width=12, justify="center")
-        table.add_column("Done", min_width=6, justify="center")
+        table = self.createTable()
 
         for todo in todos:
-            is_done_str = '✅' if todo.done == True else '❌'
-            done_date_str = "-" if todo.done_date == None else todo.done_date[:-7]
-            created_date_str = todo.created_date[:-7]
+            self.createRow(todo, table)
 
-            table.add_row(str(todo.position),
-                        todo.name, 
-                        todo.category, 
-                        created_date_str,
-                        done_date_str, 
-                        is_done_str)
         self.console.print(table)
         
     def do_create(self, list_name):
@@ -75,7 +67,7 @@ class TodoCLI(cmd.Cmd):
         list_name = split(list_name)[0]
 
         if db.tableExists(list_name):
-            print("List already exists!")
+            self.printError("List already exists!")
             return
 
         db.createTable(list_name)
@@ -86,7 +78,7 @@ class TodoCLI(cmd.Cmd):
         list_name = split(list_name)[0]
 
         if not db.tableExists(list_name):
-            print("List does not exists!")
+            self.printError("List does not exists!")
             return
         
         db.deleteTable(list_name)
@@ -99,7 +91,7 @@ class TodoCLI(cmd.Cmd):
     def do_add(self, args):
         """Adds a TODO to the list"""
         if not self.hasSelectedList():
-            self.printSelectError()
+            self.printError("You must select a list before!")
             return
 
         args = split(args)
@@ -113,7 +105,7 @@ class TodoCLI(cmd.Cmd):
     def do_remove(self, position):
         """Removes a TODO from the list"""
         if not self.hasSelectedList():
-            self.printSelectError()
+            self.printError("You must select a list before!")
             return
         
         position = split(position)[0]
@@ -122,41 +114,50 @@ class TodoCLI(cmd.Cmd):
         self.do_show(None)
 
     def do_done(self, position):
+        """Turns a TODO to done"""
         if not self.hasSelectedList():
-            self.printSelectError()
+            self.printError("You must select a list before!")
             return
 
-        done_date = datetime.now().strftime("%d/%m/%Y, %H:%M:%S.%f")
+        done_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S.%f")
         db.invertTaskStatus(position, done_date, self.selected_list)
 
         self.do_show(None)
 
     def do_change(self, args):
+        """Change a todo position"""
         if not self.hasSelectedList():
-            self.printSelectError()
+            self.printError("You must select a list before!")
             return
 
         args = split(args)
         old_position, new_position = args[0], args[1]
+
+        if int(new_position) > db.getMaxPosition(self.selected_list):
+            self.printError("New position is out of range!")
+            return
 
         db.changeTaskPosition(old_position, new_position, self.selected_list)
         self.do_show(None)
 
     
     def do_quit(self, arg):
-        """Quit the program"""
         return True
     
     def do_help(self, arg):
-        """Available commands"""
-        print("teste")        
-        """
-        if arg:
-            doc = getattr(self, "do_" + arg).__doc__
-            return doc + '\n\nDescrição:\n\tAdiciona uma saudação ao nome especificado'
-        else:
-            cmd.Cmd.do_help(self, arg)
-        """
+        self.console.print("COMMANDS:", style="bold yellow")
+        hello = """
+        lists                   List all Todo lists
+        create <name>           Create a new list
+        delete <list>           Delete a list
+        select <list>           Select a list to interact with
+        show [category]         Show the whole list (or a specific category)
+        add <todo> <category>   Add a new Todo to the list
+        remove <position>       Remove a todo with the specified position
+        done <position>         Change the status of the todo to done or undone
+        change <pos> <new_pos>  Change a todo's position
+        quit                    Quit the program"""
+        print(hello)
         
     def default(self, line):
         cmd, arg, line = self.parseline(line)
@@ -172,8 +173,35 @@ class TodoCLI(cmd.Cmd):
         
         return False
     
-    def printSelectError(self):
-        print("[ERROR] You must select a list before!")
+    def createTable(self):
+        table = Table(show_header=True, header_style="bold cyan")
+        table.add_column("#", style="dim", width=4, justify="center")
+        table.add_column("Todo", min_width=20, justify="center")
+        table.add_column("Category", min_width=12, justify="center")
+        table.add_column("Created At", min_width=12, justify="center")
+        table.add_column("Done At", min_width=12, justify="center")
+        table.add_column("Done", width=4, justify="center")
+
+        return table
+    
+    def createRow(self, todo, table):
+        is_done_str = '√' if todo.done == True else 'X'
+        done_date_str = "-" if todo.done_date == None else todo.done_date[:-7]
+        created_date_str = todo.created_date[:-7]
+
+        table.add_row(str(todo.position),
+                    todo.name, 
+                    todo.category, 
+                    created_date_str,
+                    done_date_str, 
+                    is_done_str)
+    
+    def printMessage(self, msg):
+        self.console.print(f"[OK] {msg}", style="bold cyan")
+
+    def printError(self, msg):
+        self.console.print(f"[ERROR] {msg}", style="bold red")
     
 if __name__ == '__main__':
+    Console().print("CLTodo", style="bold cyan")
     TodoCLI().cmdloop()
